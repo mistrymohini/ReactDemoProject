b2767b5eb168b74699d87b0486af52b6c55fd003
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-02-15 23:16:12 -0500

    Call Incrementtofive and for that use prevState to resolve problem

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -10,15 +10,30 @@ class Message extends Component{
     }
     increment()
     {
-        this.setState({
-           count:this.state.count+1
-        })
+        //// this will not work when incrementtofive calls it five time
+        //// as it runs all on one go
+        // this.setState({
+        //    count:this.state.count+1
+        // })
+
+        //// save result to previous state and use them to increment 1
+        this.setState(prevState=> ({
+               count: prevState.count+1
+            }))
+    }
+    incrementtofive()
+    {
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
     }
     render(){
         return(
             <div>
                 <h2> Count: {this.state.count}</h2>
-                <button onClick={() =>this.increment()}>Increment</button>
+                <button onClick={() =>this.incrementtofive()}>Increment</button>
             </div>
         )
     }
