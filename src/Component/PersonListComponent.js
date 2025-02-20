b1bc29229f694fb996f95613eacef36f67c1e4c5
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-02-19 23:40:57 -0500

    Use index as  a key when mo unique identifier

--- src/Component/PersonListComponent.js
+++ src/Component/PersonListComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 export default function PersonListComponent() {
-const names=['Mohini','Dhyana']
+const names=['Mohini','Dhyana','Mohini']
 
     const persons=[{
         id:1,
@@ -17,7 +17,7 @@ const names=['Mohini','Dhyana']
 
     const personList=persons.map(person=> <Person key={person.id} person={person}/>)
 
-    const nameList=names.map(name=> <h2 key={name} >{name}</h2>)
+    const nameList=names.map((name,index)=> <h2 key={index} >{name}</h2>)
 
   return (
     <div>{nameList}</div>
