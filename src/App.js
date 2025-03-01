8536a6812154ba0578d755ce1adcbfc27970a99e
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-02-28 21:54:03 -0500

    Removed unused component reference

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './Component/Greet';
-import Welcome from './Component/Welcome';
-import Message from './Component/Message';
-import Counter from './Component/Counter';
-import FunctionClick from './Component/FunctionClick';
-import ClassClick from './Component/ClassClick';
-import ParentComponent from './Component/ParentComponent';
-import ConditionalComponent from './Component/ConditionalComponent';
-import PersonListComponent from './Component/PersonListComponent';
-import Form from './Component/Form';
-import LifecycleA from './Component/LifecycleA';
-import Table from './Component/Table';
 
 class App extends Component {
   render(){
