88b0dde9fdb8ce4488e49cca471aeee4a0cee4db
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-03-01 21:21:02 -0500

    Created call back reference  call without current key word

--- src/Component/RefsDemo.js
+++ src/Component/RefsDemo.js
@@ -4,12 +4,19 @@ class RefsDemo extends Component {
     constructor(props) {
       super(props)
       this.inputRef=React.createRef();
+      this.cbRef=null
+      this.setcbRef= element=>{
+        this.cbRef=element
+      }
     }
 
     componentDidMount()
     {
-        this.inputRef.current.focus();
-        console.log(this.inputRef);
+        if(this.cbRef){
+            this.cbRef.focus();
+        }
+        // this.inputRef.current.focus();
+        // console.log(this.inputRef);
     }
     clickHandler=()=>{
         alert(this.inputRef.current.value)
@@ -18,6 +25,7 @@ class RefsDemo extends Component {
     return (
       <div>
         <input type='text' ref={this.inputRef}></input>
+        <input type='text' ref={this.setcbRef}></input>
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
