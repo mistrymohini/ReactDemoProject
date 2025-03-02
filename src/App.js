c5462866d42edb8c3d446e15da3d41779f9372c3
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-03-01 21:41:09 -0500

    Created a reference using class component by calling child component ref method from Parent Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import RefsDemo from './Component/RefsDemo';
+import FocusInput from './Component/FocusInput';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-       <RefsDemo/>
+       <FocusInput/>
       </div>
     );
   }
---
+++ src/Component/Input.js
@@ -0,0 +1,19 @@
+import React, { Component, createRef } from 'react'
+
+class Input extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.inputRef=createRef();
+    }
+    focusInput(){
+        this.inputRef.current.focus()
+    }
+  render() {
+    return (
+      <input type='text' ref={this.inputRef}/>
+    )
+  }
+}
+
+export default Input
