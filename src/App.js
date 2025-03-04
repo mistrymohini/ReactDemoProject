9dc015047dbaab295a70bac38ffb1619e4f1da74
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-03-03 23:08:29 -0500

    Error Boundary using its methods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FRParentInput from './Component/FRParentInput';
+import ErrorShow from './Component/ErrorShow';
+import ErrorBoundry from './Component/ErrorBoundry';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-       <FRParentInput/>
+        <ErrorBoundry>
+        <ErrorShow heroName="Batman"/>
+        </ErrorBoundry>
+        <ErrorBoundry>
+        <ErrorShow heroName="Spiderman"/>
+        </ErrorBoundry>
+        <ErrorBoundry>
+        <ErrorShow heroName="Joker"/>
+        </ErrorBoundry>
       </div>
     );
   }
