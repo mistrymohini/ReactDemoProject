c2cec17bdea6468a3b62b5dbd478f59e6da960ab
Mohini <58016042+mistrymohini@users.noreply.github.com>
2025-03-06 21:24:47 -0500

    Created Higher Order comonent and use it in a ClickCounter and HoverCounter Component. HOC taking care of state and maintaining incrementCount

--- src/Component/ClickCounter.js
+++ src/Component/ClickCounter.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react'
+import updatedComponent from './WithCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-      super(props)
     
-      this.state = {
-         count:0
-      }
-    }
-    incrementCount= () =>{
-        this.setState(prevState =>{
-            return { count:prevState.count + 1 }
-        })
-    }
   render() {
-    const {count}=this.state
+    const {count,incrementCount}=this.props
     return (
-      <button onClick={this.incrementCount}>Clicked {count} times </button>
+      <button onClick={incrementCount}>Clicked {count} times </button>
     )
   }
 }
 
-export default ClickCounter
+export default updatedComponent(ClickCounter)
--- src/Component/HoverCounter.js
+++ src/Component/HoverCounter.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react'
+import updatedComponent from './WithCounter'
 
 class HoverCounter extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            count: 0
-        }
-    }
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1 }
-        })
-    }
     render() {
+    const {count,incrementCount}=this.props
+
         return (
-            <h1 onMouseOver={this.incrementCount}>Hover {this.state.count} times</h1>
+            <h1 onMouseOver={incrementCount}>Hover {count} times</h1>
         )
     }
 }
 
-export default HoverCounter
+export default updatedComponent(HoverCounter)
